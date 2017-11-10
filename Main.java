d3af3ce890eb753ef2b7aab14bdbc1ed27abafd4
joshuacassidy <Joshcassidy79@gmail.com>
2017-11-10 23:48:50 +0000

    added methods for recursive selection sorting

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         SelectionSort<Integer> selectionSort = new SelectionSort<>();
-        selectionSort.showArray(selectionSort.sort(new Integer[]{3,6,0,-5,5,7,2,1} ));
+        selectionSort.showArray(selectionSort.recursiveSort(new Integer[]{3,6,0,-5,5,7,2,1},0 ));
 
     }
 }
--- SelectionSort.java
+++ SelectionSort.java
@@ -3,12 +3,11 @@ public class SelectionSort<T extends Comparable<T>> {
     public T[] sort(T[] listOfItems) {
         for (int i = 0; i < listOfItems.length-1; i++) {
             int index = i;
-            for (int j = i+1; j < listOfItems.length; j++) {
+            for (int j = index+1; j < listOfItems.length; j++) {
                 if (listOfItems[j].compareTo(listOfItems[index]) < 0){
                     index = j;
                 }
             }
-
             if (index != i) {
                 T temp = listOfItems[i];
                 listOfItems[i] = listOfItems[index];
@@ -18,6 +17,31 @@ public class SelectionSort<T extends Comparable<T>> {
         }
         return listOfItems;
     }
+
+    public T[] recursiveSort(T[] listOfItems, int i) {
+        if (i < listOfItems.length) {
+            int index = recursiveSubSort(listOfItems,i,i+1);
+            if (index != i) {
+                T temp = listOfItems[i];
+                listOfItems[i] = listOfItems[index];
+                listOfItems[index] = temp;
+            }
+            return recursiveSort(listOfItems, i+1);
+        }
+
+        return listOfItems;
+    }
+
+    public int recursiveSubSort(T[] listOfItems, int index, int j) {
+        if (j < listOfItems.length){
+            if (listOfItems[j].compareTo(listOfItems[index]) < 0){
+                index = j;
+            }
+            return recursiveSubSort(listOfItems,index,j+1);
+        }
+        return index;
+    }
+
 
     public void showArray(Integer[] listOfItems){
         for(int i: listOfItems) {
